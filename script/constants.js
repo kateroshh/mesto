0f618ae6556e6536a08229fbd6f4f45d53dcda3c
Kate Roshchupkina <katerina.ermakov@mail.ru>
2023-05-19 16:33:34 +0300

    Fix comment 1

---
+++ script/constants.js
@@ -0,0 +1,28 @@
+const galleryItemTemplate = document.querySelector('.gallery-item-template').content; //темплейт для карточки фото и подпись
+const galleryItems = document.querySelector('.gallery-items'); //место куда добавляем карточки
+
+const buttonEditProfile = document.querySelector('.profile-info__edit'); //кнопка редактирование профиля
+const nameText = document.querySelector('.profile-info__nametext'); //текстовый элемент имя
+const descriptionText = document.querySelector('.profile-info__description'); //текстовый элемент описание
+
+const popup = document.querySelector('.popup'); //модальное окно
+const buttonCloseEditForm = document.querySelector('.popup__close_edit'); //кнопка крестик
+const popupEdit = document.querySelector('.popup-edit'); //модальное окно
+const formElementEdit = popupEdit.querySelector('.popup-form'); //форма редактирования профиля
+const nameInputEdit = formElementEdit.querySelector('#name'); //поле редактирования имени
+const descriptionInputEdit = formElementEdit.querySelector('#description'); //поле редактирования описания
+const errorTextEdit = popupEdit.querySelector('.popup__error'); //сообщение об ошибке
+
+const buttonCloseCreateForm = document.querySelector('.popup__close_create'); //кнопка крестик
+const buttonCreate = document.querySelector('.profile__add'); //кнопка создание новой карточки
+const popupCreate = document.querySelector('.popup-create'); //модальное окно создания карточки
+const formElementCreate = popupCreate.querySelector('.popup-form'); //форма создания карточки
+const titleInputCreate = formElementCreate.querySelector('#title-card'); //поле заголовка карточки
+const linkInputCreate = formElementCreate.querySelector('#link-card'); //поле ссылка на картинку
+const errorTextCreate = popupCreate.querySelector('.popup__error'); //сообщение об ошибке
+
+
+const popupPhoto = document.querySelector('.popup-photo'); //модальное окно фото
+const buttonClosePhoto = document.querySelector('.popup-photo__close'); //кнопка крестик
+const imgPhoto = document.querySelector('.popup-photo__img'); //картинка
+const descriptionTextPhoto = document.querySelector('.popup-photo__description'); //текст описание картинки
--- script/index.js
+++ script/index.js
@@ -1,76 +1,25 @@
-//Исходный массив фото с подписями
-const initialCards = [
-  {
-    name: 'Islands of NEOM, Saudi Arabia',
-    link: 'https://images.unsplash.com/photo-1682687982107-14492010e05e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
-  },
-  {
-    name: 'Египет',
-    link: 'https://plus.unsplash.com/premium_photo-1661841439995-1706237c83dc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80'
-  },
-  {
-    name: 'Италия',
-    link: 'https://images.unsplash.com/photo-1683654968102-da4bf0d2d179?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=715&q=80'
-  },
-  {
-    name: 'Al Noor Island in Sharjah on DJI Mavic 2 // Sharjah, UAE',
-    link: 'https://images.unsplash.com/photo-1683491184388-7e8ebb14ac31?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80'
-  },
-  {
-    name: 'Китай',
-    link: 'https://images.unsplash.com/photo-1684033705603-32a9bf9b7ddf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
-  },
-  {
-    name: 'Индонезия',
-    link: 'https://images.unsplash.com/photo-1542548124-c0a04217fcbc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80'
-  }
-];
-
-const galleryItemTemplate = document.querySelector('.gallery-item-template').content; //темплейт для карточки фото и подпись
-const galleryItems = document.querySelector('.gallery-items'); //место куда добавляем карточки
-
-const editButton = document.querySelector('.profile-info__edit'); //кнопка редактирование профиля
-const nameText = document.querySelector('.profile-info__nametext'); //текстовый элемент имя
-const descriptionText = document.querySelector('.profile-info__description'); //текстовый элемент описание
-
-const popup = document.querySelector('.popup'); //модальное окно
-const closeButtonEdit = document.querySelector('.popup__close_edit'); //кнопка крестик
-const popupEdit = document.querySelector('.popup-edit'); //модальное окно
-const formElementEdit = popupEdit.querySelector('.popup-form'); //форма редактирования профиля
-const nameInputEdit = formElementEdit.querySelector('#name'); //поле редактирования имени
-const descriptionInputEdit = formElementEdit.querySelector('#description'); //поле редактирования описания
-const errorTextEdit = popupEdit.querySelector('.popup__error'); //сообщение об ошибке
-
-const closeButtonCreate = document.querySelector('.popup__close_create'); //кнопка крестик
-const createButton = document.querySelector('.profile__add'); //кнопка создание новой карточки
-const popupCreate = document.querySelector('.popup-create'); //модальное окно создания карточки
-const formElementCreate = popupCreate.querySelector('.popup-form'); //форма создания карточки
-const titleInputCreate = formElementCreate.querySelector('#title-card'); //поле заголовка карточки
-const linkInputCreate = formElementCreate.querySelector('#link-card'); //поле ссылка на картинку
-const errorTextCreate = popupCreate.querySelector('.popup__error'); //сообщение об ошибке
-
-
-const popupPhoto = document.querySelector('.popup-photo'); //модальное окно фото
-const closeButtonPhoto = document.querySelector('.popup-photo__close'); //кнопка крестик
-const imgPhoto = document.querySelector('.popup-photo__img'); //картинка
-const descriptionTextPhoto = document.querySelector('.popup-photo__description'); //текст описание картинки
-
-//Отображение карточки и событий на карточки
-function renderCard(name, link) {
+//Создание карточки и событий на карточки
+function createCard(name, link) {
   const itemElement = galleryItemTemplate.cloneNode(true);
   itemElement.querySelector('.gallery-item__text').innerText = name;
   itemElement.querySelector('.gallery-item__img').src = link;
   itemElement.querySelector('.gallery-item__img').alt = name;
-  itemElement.querySelector('.gallery-item__img').addEventListener('click', handlegalleryPhoto);
-  itemElement.querySelector('.gallery-item__like').addEventListener('click', handlegalleryLike);
-  itemElement.querySelector('.gallery-item__delete').addEventListener('click', handlegalleryDelete);
-  galleryItems.prepend(itemElement);
+  itemElement.querySelector('.gallery-item__img').addEventListener('click', handleGalleryPhoto);
+  itemElement.querySelector('.gallery-item__like').addEventListener('click', handleGalleryLike);
+  itemElement.querySelector('.gallery-item__delete').addEventListener('click', handleGalleryDelete);
+  renderCard(itemElement);
+  return itemElement;
+}
+
+//Добавление карточки на страницу
+function renderCard(card) {
+  galleryItems.prepend(card);
 }
 
 //Загрузка карточек при открытие сайта
 function initGallery(items) {
   items.forEach(item => {
-    renderCard(item.name, item.link);
+    createCard(item.name, item.link);
   });
 }
 
@@ -132,13 +81,14 @@ function handleFormSubmitCreate (evt) {
 
   if(titleInputCreate.value != null && titleInputCreate.value.trim() != '' && linkInputCreate.value != null && linkInputCreate.value.trim() != '')
   {
-    renderCard(titleInputCreate.value, linkInputCreate.value);
+    createCard(titleInputCreate.value, linkInputCreate.value);
 
     //Закрытие модального окна
     handleClosePopup();
 
-    titleInputCreate.value = '';
-    linkInputCreate.value = '';
+    //Очистка полей формы
+    formElementCreate.reset();
+
   } else {
     //Проверка на заполнение полей
     errorTextCreate.classList.add('popup__error_active');
@@ -158,16 +108,16 @@ function handleFormSubmitCreate (evt) {
 }
 
 //Нажатие на кнопку Нравится
-function handlegalleryLike (event) {
+function handleGalleryLike (event) {
   event.target.classList.toggle('gallery-item__like_active');
 }
 
 //Кнопка удаления карточки (мусорка)
-function handlegalleryDelete (event) {
+function handleGalleryDelete (event) {
   event.target.closest('.gallery-item').remove();
 }
 
-function handlegalleryPhoto(event) {
+function handleGalleryPhoto(event) {
   imgPhoto.src = event.target.src;
   imgPhoto.alt = event.target.alt;
   descriptionTextPhoto.textContent = event.target.alt;
@@ -179,18 +129,18 @@ function handlegalleryPhoto(event) {
 initGallery(initialCards);
 
 //Закрытие при нажатие на крестик
-closeButtonEdit.addEventListener('click', handleClosePopup);
-closeButtonCreate.addEventListener('click', handleClosePopup);
-closeButtonPhoto.addEventListener('click', handleClosePopup);
+buttonCloseEditForm.addEventListener('click', handleClosePopup);
+buttonCloseCreateForm.addEventListener('click', handleClosePopup);
+buttonClosePhoto.addEventListener('click', handleClosePopup);
 
 //Нажатие на кнопку редактировать
-editButton.addEventListener('click', handleOpenPopupEdit);
+buttonEditProfile.addEventListener('click', handleOpenPopupEdit);
 
 //Нажатие на кнопку Сохранить модального окна
 formElementEdit.addEventListener('submit', handleFormSubmitEdit);
 
 //Нажатие на кнопку (+)
-createButton.addEventListener('click', handleOpenPopupCreate);
+buttonCreate.addEventListener('click', handleOpenPopupCreate);
 
 //Нажатие на кнопку Создать модальное окно
 formElementCreate.addEventListener('submit', handleFormSubmitCreate);
